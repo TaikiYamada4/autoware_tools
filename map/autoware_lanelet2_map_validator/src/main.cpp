7e7ddf00055f95a4966611b05e244b4265fe16f2
TaikiYamada4 <taiki.yamada@tier4.jp>
2024-10-25 17:03:26 +0900

    Fixed 'prequisite' to 'prerequisite'
    Signed-off-by: TaikiYamada4 <taiki.yamada@tier4.jp>

--- map/autoware_lanelet2_map_validator/src/main.cpp
+++ map/autoware_lanelet2_map_validator/src/main.cpp
@@ -54,8 +54,8 @@ Validators parse_validators(const json & json_data)
     for (const auto & validator : requirement["validators"]) {
       ValidatorInfo info;
 
-      if (validator.contains("prequisite")) {
-        for (const auto & prereq : validator["prequisite"]) {
+      if (validator.contains("prerequisite")) {
+        for (const auto & prereq : validator["prerequisite"]) {
           info.prerequisites.push_back(prereq["name"]);
           if (prereq.contains("forgive_warnings")) {
             info.forgive_warnings[prereq["name"]] = prereq["forgive_warnings"];
